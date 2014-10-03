7e14cba458c3b05dd553986cd8cc08f3e49179af
podypodpod <podypodpod@gmail.com>
2014-10-03 15:55:03 +0100

    Still working on GUI pssing checkers in to panel

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -13,7 +13,10 @@ public class BoardPanel extends JPanel{
 	Ellipse2D.Float[] redCheckers = new Ellipse2D.Float[15];
 	Ellipse2D.Float[] blackCheckers = new Ellipse2D.Float[15];
 	
-	/**
+	int redCount = 0;
+	int blackCount = 0;
+	
+	/**`
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
@@ -37,31 +40,52 @@ public class BoardPanel extends JPanel{
 	    g2.drawImage(imgBG, 0, 0, this);
 	    
 	    //Drawing Red checkers
-	    g2.setColor(Color.RED);
 	    for(int i = 0; i<redCheckers.length;i++){
+	    	g2.setColor(Color.RED);
 	    	g2.fill(redCheckers[i]);
+	    	g2.setColor(Color.WHITE);
+	    	g2.draw(redCheckers[i]);
 	    }
 	    //Drawing Black checkers
-	    g2.setColor(Color.BLACK);
+	    
 	    for(int i = 0; i<blackCheckers.length;i++){
+	    	g2.setColor(Color.BLACK);
 	    	g2.fill(blackCheckers[i]);
+	    	g2.setColor(Color.WHITE);
+	    	g2.draw(blackCheckers[i]);
 	    }
 		
 	}
 	
 	public void printCheckers(int point, int amount, boolean color){
-/**
+		if(redCount == 14){redCount=0;}
+		if(blackCount == 14){blackCount=0;}	
+		
 		//if its one of the 0 points
-		if(point==0||point==25){
-			
+		if((point==0||point==25)&&amount!=0){
+			if(color == true){
+				//X CORD NEEDS TO BE ABOUT MIDDLE
+				blackCheckers[blackCount] = new Ellipse2D.Float(510,100,50,50);
+			}else{
+				//X CORD NEEDS TO BE ABOUT MIDDLE
+				redCheckers[redCount] = new Ellipse2D.Float(510,400,50,50);
+			}
 		//if its the top half of the board
 		}else if(point<25&&point>0){
-			
+			if(color == true){
+				
+			}else{
+				
+			}
 		//if its the bottom half
 		}else{
-			
+			if(color == true){
+				
+			}else{
+				
+			}
 		}
-*/
+
 	}
 	
 	
